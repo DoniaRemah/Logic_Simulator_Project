0f0bf165f26567a6617206a21b029be8177a6c04
ola abouelhadid <abouelhadid.ola@gmail.com>
2020-11-29 19:48:31 +0200

    Get user click(toolbar1, toolbar2)

--- Phase1 - Code/GUI/Input.cpp
+++ Phase1 - Code/GUI/Input.cpp
@@ -30,23 +30,78 @@ ActionType Input::GetUserAction() const
 
 	if(UI.AppMode == DESIGN )	//application is in design mode
 	{
-		//[1] If user clicks on the Toolbar
-		if ( y >= 0 && y < UI.ToolBarHeight)
-		{	
+		//[1] If user clicks on the Toolbar1(GATES)
+		
 			//Check whick Menu item was clicked
 			//==> This assumes that menu items are lined up horizontally <==
 			int ClickedItemOrder = (x / UI.ToolItemWidth);
+				switch (ClickedItemOrder)
+				{
+				case ITM_BUFFER: return ADD_Buff;
+				case ITM_NOT: return ADD_INV;
+				case ITM_AND2: return ADD_AND_GATE_2;
+				case ITM_AND3: return ADD_AND_GATE_3;
+				case ITM_OR2: return ADD_OR_GATE_2;
+				case ITM_NAND2: return ADD_NAND_GATE_2;
+				case ITM_NOR2: return ADD_NOR_GATE_2;
+				case ITM_XOR2: return ADD_XOR_GATE_2;
+				case ITM_XNOR2: return ADD_XNOR_GATE_2;
+				case ITM_NOR3: return ADD_NOR_GATE_3;
+				case ITM_XOR3: return ADD_XOR_GATE_3;
+				case ITM_SWITCH: return ADD_Switch;
+				case ITM_BULB: return ADD_LED;
+				case ITM_WIRE: return ADD_CONNECTION;
+				default: return DSN_TOOL;	//A click on empty place in desgin toolbar
+				}
+			// IF THE USER CLICKS ON TOOLBAR2
+				if (y >= 0 && y < UI.ToolBarHeight)
+				{
+					if (y >= UI.ToolBarHeight && y < UI.ToolBarHeight2)
+					{
+
+						int ClickedItemOrder = (x / UI.ToolItemWidth);
+						if (0 <= ClickedItemOrder && ClickedItemOrder < (7 * UI.ToolItemWidth2))
+
+						{
+							switch (ClickedItemOrder)
+							{
+							case ITM_LABEL: return ADD_Label;
+							case ITM_EDIT: return EDIT_Label;
+							case ITM_CUT: return CUT;
+							case ITM_COPY: return COPY;
+							case ITM_PASTE: return PASTE;
+							case ITM_LOAD: return LOAD;
+							case ITM_SAVE: return SAVE;
+							
+
+
+							default: return DSN_TOOL;
+
+							}
+						}
+						else if (ClickedItemOrder > (7 * UI.ToolItemWidth2) && ClickedItemOrder < (2 * UI.ToolItemWidth3))
+						{
+							switch (ClickedItemOrder)
+							{
+							case ITM_MULTI_SELECT: return MULTI_SELECT;
+							case ITM_SWITCH_TO_SIM: return SIM_MODE;
+							default: return DSN_TOOL;
+
+							}
+						}
+						else if (ClickedItemOrder > (2 * UI.ToolItemWidth3) && ClickedItemOrder < UI.width)
+						{
+							EXIT;
+						}
+					}
 			//Divide x coord of the point clicked by the menu item width (int division)
 			//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
 
-			switch (ClickedItemOrder)
-			{
-			case ITM_AND2: return ADD_AND_GATE_2;
-			case ITM_OR2: return ADD_OR_GATE_2;
-			default: return DSN_TOOL;	//A click on empty place in desgin toolbar
-			}
+			
 		}
 	
+	
+
 		//[2] User clicks on the drawing area
 		if ( y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight)
 		{
