bec16711ca48385e5ab5580fcd95996f8c5e7d08
Saykoyes <75132780+Saykoyes@users.noreply.github.com>
2021-01-05 06:59:08 -0800

    Update AddNORgate2.cpp

--- Project Code Framework/Actions/AddNORgate2.cpp
+++ Project Code Framework/Actions/AddNORgate2.cpp
@@ -23,9 +23,16 @@ bool AddNORgate2::ReadActionParameters()
 		pOut->PrintMsg("Invalid position. Operation was cancelled");
 		return false;
 	}
-	//Clear Status Bar
-	pOut->ClearStatusBar();
-	return true;
+		if (pManager->selectcomponent(Cx, Cy))
+	{
+		pOut->PrintMsg("Invalid position. Operation was cancelled");
+		return false;
+	}
+	else
+	{
+		pOut->ClearStatusBar();
+		return true;
+	}
 
 }
 
