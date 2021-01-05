7e6a624bc897699ba58146d76a3a0e9225ed829d
Saykoyes <75132780+Saykoyes@users.noreply.github.com>
2021-01-05 07:04:06 -0800

    Update EDIT.cpp

--- Project Code Framework/Actions/EDIT.cpp
+++ Project Code Framework/Actions/EDIT.cpp
@@ -16,25 +16,49 @@ bool EDIT::ReadActionParameters()
 	Input* pIn = pManager->GetInput();
 
 	//Print Action Message
-	pOut->PrintMsg("Edit the label of component: ");
-	
+	pOut->PrintMsg("Edit the label of component:select the component");
+
 	//Wait for User Input
 	pIn->GetPointClicked(Cx, Cy);
-	//Clear Status Bar
-	pOut->ClearStatusBar();
-	return true;
+	if (!pOut->IsDrawingArea(Cx, Cy)) {
+		pOut->PrintMsg("Invalid position. Operation was cancelled");
+		return false;
+	}
+	if (pManager->selectcomponent(Cx, Cy))
+	{
+		mComp = pManager->GetComponent(Cx, Cy);
+		privlabel = mComp->GetLabel();
+		if (!privlabel.empty())
+		{
+			Editlabel = pIn->GetSrting(pOut,"edit the component:",privlabel);
+			return true;
+		}
+		else
+		{
+			pOut->PrintMsg("please put the label first then edit it ");
+			return false;
+		}
+	}
+	else
+	{
+		pOut->PrintMsg("Invalid position. Operation was cancelled");
+		return false;
+	}
 }
+	
+	
 
 void EDIT::Execute()
 {
+	Output* pOut = pManager->GetOutput();
 	//Get Center point of the Gate
-	ReadActionParameters();
-
-	//Calculate the rectangle Corners
-	
-
-	GraphicsInfo GInfo; //Gfx info to be used to construct the EDIT
-	
+	bool x=ReadActionParameters();
+	if (x)
+	{
+		GraphicsInfo GInfo = mComp->GetGfxInfo();//Gfx info to be used to construct the EDIT
+		pOut->Cleartextindrawingarea(GInfo.x1, (GInfo.y1 - 20));
+		pOut->DrawString(GInfo.x1, GInfo.y1 - 20, Editlabel);
+	}
 }
 
 void EDIT::Undo()
@@ -42,5 +66,4 @@ void EDIT::Undo()
 
 void EDIT::Redo()
 {}
-
 
