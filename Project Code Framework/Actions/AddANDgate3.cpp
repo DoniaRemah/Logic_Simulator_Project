1d280d09c63f6ce0824adf399dac61feadd6e164
Saykoyes <75132780+Saykoyes@users.noreply.github.com>
2021-01-05 06:57:35 -0800

    Update AddANDgate3.cpp

--- Project Code Framework/Actions/AddANDgate3.cpp
+++ Project Code Framework/Actions/AddANDgate3.cpp
@@ -25,9 +25,16 @@ bool AddANDgate3::ReadActionParameters()
 		pOut->PrintMsg("Invalid position. Operation was cancelled");
 		return false;
 	}
-	//Clear Status Bar
-	pOut->ClearStatusBar();
-	return true;
+	if (pManager->selectcomponent(Cx, Cy))
+	{
+		pOut->PrintMsg("Invalid position. Operation was cancelled");
+		return false;
+	}
+	else
+	{
+		pOut->ClearStatusBar();
+		return true;
+	}
 
 }
 
